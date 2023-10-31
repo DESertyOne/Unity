1a920df16c7cadf3e949e5ca81aad700e3bda331
Deserty <waffen576@gmail.com>
2023-11-01 01:47:08 +0500

    edit work 8

--- PracticalWork8/Assets/Scenes/Script/Run.cs
+++ PracticalWork8/Assets/Scenes/Script/Run.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class Run : MonoBehaviour
 {
     public float speed = 0.1f;
-    public int checkpoints[] =  new checkpiont;
+    public Transform[] pointone;
+    public int moveingTo = 0;
+    public int movementDerection = 1;
     
     void Start()
     {
@@ -15,6 +17,15 @@ public class Run : MonoBehaviour
 
     void Update()
     {
-        
+        if (pointone == null || pointone.Length < 1)
+        {
+            return;
+        }
+        while(true) 
+        {
+          transform.position = Vector3.MoveTowards(transform.position, pointone[pointone.Length+1].position,Time.deltaTime * speed);
+
+        }
+
     }
 }

7fa86a8710530458a7bcca2a4c3a4c729f8c1f7d
Deserty <waffen576@gmail.com>
2023-11-05 00:21:18 +0500

    edit work 8

--- PracticalWork8/Assets/Scenes/Script/Run.cs
+++ PracticalWork8/Assets/Scenes/Script/Run.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class Run : MonoBehaviour
@@ -6,31 +5,36 @@ public class Run : MonoBehaviour
     public float speed;
     public Transform[] pointone;
     public bool movementDerection;
-    private Vector3 target ;
-    private int indexTarget = 0;
-    
+    private Vector3 target;
+    public int indextarget;
+    public bool back;
+
+
 
     void Start()
     {
-        target = Transform[indexTarget].position;
+        indextarget = 0;
+        target = pointone[indextarget].position;
+
     }
 
     void Update()
-    {            
-        transform.Rotate(0, 0, 0.2f);
-        transform.LookAt(target);
+    {
+
         if (movementDerection)
         {
+            transform.Rotate(0, 0, 0.2f);
+            transform.LookAt(target);
             transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
-        }
-        if (transform.position == pointone[0].position)
-        {                    
-            target = pointone[1].position;
-        }
-        if (transform.position == pointone[1].position)
-        {
-            target = pointone[2].position;
-        }
 
+        }
+        if (back)
+        {
+            if (transform.position == pointone[indextarget].position)
+            {
+                indextarget++;
+                target = pointone[indextarget].position;
+            }
+        }
     }
 }
